15f47de9ab797ee9a95abba444b674d6cb9576a6
Redhatfour1 <yaloumessenger@yahoo.com>
2017-04-15 14:16:41 -0700

    worked on stretch goal where we added a route to handle a request and send to new html. We also added a console.log to let us know what port is runnig.

--- starter-code/server.js
+++ starter-code/server.js
@@ -11,14 +11,15 @@ const PORT = process.env.PORT || 3000;
 
 // TODO: Include all of the static resources as an argument to app.use()
 app.use(express.static(__dirname + '/public'));
-
+// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+app.get('/', function(req, res) {
+  res.sendFile(__dirname + '/index.html');
+});
 app.get('*', function(req, res) {
   res.sendFile(__dirname + '/index.html');
   // TODO: Using the response object, send the index.html file back to the user
 });
-
-// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-
 app.listen(PORT, function() {
   // TODO: Log to the console a message that lets you know which port your server has started on
+  console.log('server running on port: ' + PORT);
 });
