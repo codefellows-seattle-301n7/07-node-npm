a6467ea3bc60df69092e13c5243feb4d78c792cb
ibis0503 <kennyhong@outlook.com>
2017-04-15 13:30:27 -0700

    populate package.json

--- starter-code/server.js
+++ starter-code/server.js
@@ -1,10 +1,12 @@
 'use strict';
 
 // TODO: Initialize your project using NPM to create and populate a package.json file
+// Done: Initialize your project using NPM to create and populate a package.json file
 
 // TODO: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
-
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // TODO: Include all of the static resources as an argument to app.use()
